71ee5fec1a519cad87882157b2834872b9144084
Lukáš Plevač <lukasplevac@gmail.com>
2021-02-07 20:23:21 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@ rm -rf airootfs/to_install
 mkdir airootfs/to_install
 mkdir airootfs/to_install/etc
 mkdir airootfs/to_install/usr
+mkdir airootfs/to_install/usr/local
 
 touch airootfs/to_install/do_not_edit
 cp packages.x86_64 airootfs/to_install
@@ -11,5 +12,7 @@ cp airootfs/etc/os-release airootfs/to_install/etc
 cp airootfs/etc/bash.bashrc airootfs/to_install/etc
 cp -r airootfs/etc/pacman.d airootfs/to_install/etc
 cp -r airootfs/usr/backgrounds airootfs/to_install/usr
+
+cp -r airootfs/usr/local/bin airootfs/to_install/usr/local
 
 mkarchiso -v -o ${PWD}/out ${PWD}
